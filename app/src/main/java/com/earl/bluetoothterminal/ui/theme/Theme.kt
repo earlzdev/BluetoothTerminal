a50d2337b4bdca71e831770f4ec5952fbf2dd24f
earl <esinilyadev@gmail.com>
2023-07-13 00:09:59 +0500

    initial commit

--- app/src/main/java/com/earl/bluetoothterminal/ui/theme/Theme.kt
+++ app/src/main/java/com/earl/bluetoothterminal/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun BluetoothTerminalTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun BluetoothTerminalTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
